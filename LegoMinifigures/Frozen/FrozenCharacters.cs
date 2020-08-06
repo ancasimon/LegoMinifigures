81d1142385ed6f5ea9686272032598a83eb7611b
Anca Simon <ancarsimon@gmail.com>
2020-08-05 21:21:51 -0500

    Add project files.

---
+++ LegoMinifigures/Frozen/FrozenCharacters.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures.Frozen
+{
+    class FrozenCharacters
+    {
+    }
+}
---
+++ LegoMinifigures/Moana/MoanaCharacters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures.Moana
+{
+    class MoanaCharacters
+    {
+        //properties:
+        public HairColor CharacterHairColor { get; }
+    }
+
+    enum HairColor
+    {
+        NotSet,
+        Black,
+        Blond,
+        Brown,
+        Red
+    }
+
+    enum HairLength
+    {
+        NotSet,
+        Long,
+        Short
+    }
+
+    enum EyeColor
+    {
+        NotSet,
+        Black,
+        Blue,
+        Brown,
+        Green
+    }
+}
